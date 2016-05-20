7311e9b5c6442dfe93be28441131fa62ff026586
Baggers <chris.bagley@gmail.com>
2016-05-20 18:07:14 +0200

    export to json

---
+++ to-json.lisp
@@ -0,0 +1,14 @@
+(ql:quickload :cl-json)
+
+(in-package :glsl-spec)
+
+(defun export-to-json ()
+  (export-data-to-json glsl-spec:*functions* "functions.json")
+  (export-data-to-json glsl-spec:*variables* "variables.json"))
+
+(defun export-data-to-json (data file-name)
+  (with-open-file (x (format nil "./~a" file-name) :direction :output
+                     :if-exists :supersede)
+    (format x "[~{~a~^,~%~}]"
+            (mapcar #'cl-json:encode-json-plist-to-string
+                    data))))
