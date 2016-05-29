421a89e1d4323dd8ba7cae0872b48c2587518864
Baggers <chris.bagley@gmail.com>
2016-05-29 14:58:19 +0200

    add some missing min & max variants

--- to-json.lisp
+++ to-json.lisp
@@ -7,8 +7,8 @@
   (export-data-to-json glsl-spec:*variables* "variables.json"))
 
 (defun export-data-to-json (data file-name)
-  (with-open-file (x (format nil "./~a" file-name) :direction :output
-                     :if-exists :supersede)
+  (with-open-file (x (asdf:system-relative-pathname :glsl-spec file-name)
+		     :direction :output :if-exists :supersede)
     (format x "[~{~a~^,~%~}]"
             (mapcar #'cl-json:encode-json-plist-to-string
                     data))))
